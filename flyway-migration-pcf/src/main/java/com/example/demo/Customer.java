2ce2e72aaba22ca262eb32d7014b79290fa0cde5
Ghalan Tamang <167817@NTTDATA.COM>
2018-12-19 05:42:04 -0600

    changes

--- flyway-migration-pcf/src/main/java/com/example/demo/Customer.java
+++ flyway-migration-pcf/src/main/java/com/example/demo/Customer.java
@@ -12,34 +12,38 @@ public class Customer {
     private Long id;
 
     private String name;
-
-    @JsonIgnore
-    @OneToMany (mappedBy = "customer")
-    private List<Contact> contacts;
-
+    private String contactName;
+    private String email;
+    private String phone;
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public List<Contact> getContacts() {
-		return contacts;
+	public String getContactName() {
+		return contactName;
+	}
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 
-	public void setContacts(List<Contact> contacts) {
-		this.contacts = contacts;
-	}
-
-    
 }
